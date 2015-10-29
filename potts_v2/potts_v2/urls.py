7bc75b85b3913378fc74695d99aa56455830c527
Tran <tranlong@ua41f726c091f55e9d344.ant.amazon.com>
2015-10-29 14:21:59 -0700

    css added

--- potts_v2/potts_v2/urls.py
+++ potts_v2/potts_v2/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from potts.views import DashboardView
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', DashboardView.as_view()),
 ]
