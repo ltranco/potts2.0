e80266f8b207ffa7cb49b6cac098d79f45a03ac8
Tran <tranlong@ua41f726c091f55e9d344.ant.amazon.com>
2015-10-29 14:02:30 -0700

    project structure init

--- potts_v2/potts_v2/urls.py
+++ potts_v2/potts_v2/urls.py
@@ -1,4 +1,4 @@
-"""potts URL Configuration
+"""potts_v2 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.8/topics/http/urls/
--- potts_v2/potts_v2/wsgi.py
+++ potts_v2/potts_v2/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for potts project.
+WSGI config for potts_v2 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "potts.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "potts_v2.settings")
 
 application = get_wsgi_application()
